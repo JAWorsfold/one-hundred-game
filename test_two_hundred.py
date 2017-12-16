3f1e19afb7934a2877c72c1c06e2b14f353a6ea1
jworsf01 <32646818+jworsf01@users.noreply.github.com>
2017-12-16 11:53:54 +0000

    Added additional functionality to make the computer act more intelligently

--- test_two_hundred.py
+++ test_two_hundred.py
@@ -20,7 +20,13 @@ def test_roll():
     assert roll() <= 6
     assert roll() > 0
 
+# possibly get rid of this after refactoring
+
 
 def test_roll_calc():
     assert roll_calc(roll()) <= 6
     assert roll_calc(roll()) >= 0
+
+# add test for the major functions - computer and human move
+# Work out how to use a random seed
+# Can I test the move now as I've added lots of print statements? Yes...
--- two_hundred.py
+++ two_hundred.py
@@ -1,4 +1,4 @@
-# Joseph Worsfold
+# Joseph Worsfold - jworsf01
 # Executes the two-hundred game - see instructions for more details
 
 import random
@@ -6,28 +6,67 @@ import random
 
 def instructions():
     """Tells the user the rules of the game."""
-    print('The instructions')
+    # add something to make it look nicer
+    print('Welcome to the "Hundred" game.\nThe aim of the game is to be the '
+          'first to reach a score of 100.\nEach turn you will roll a six '
+          'sided die and add the result to\nyour score. However, if you roll '
+          'a 1 your score for that turn will\nbe 0. You will play against the'
+          ' computer who will roll first.')
+    #  use the %s method to add the nicer layout
 
 
 def computer_move(computer_score, human_score):
     """The computer rolls a random number of times, displays the result of
     each roll, and returns the result (either 0 or the total of the rolls)."""
-    num_of_rolls = random.randint(1, 6)
-
+    print(' \nThis is the computer\'s turn:')
     roll_total = 0
-    while num_of_rolls != 0:
-        roll_score = roll_calc(roll())
-        if roll_score == 0:
-            return roll_score
-        roll_total += roll_score
-        num_of_rolls -= 1
-    return roll_total
+    if computer_score >= human_score:
+        while roll_total < 11:
+            roll_score = roll_calc(roll())
+            if roll_score == 0:
+                print('The computer\'s score at the end of the turn is 0.')
+                return 0
+            roll_total += roll_score
+        print('The computer\'s score at the end of the turn is '
+              + str(roll_total) + '.')
+        return roll_total
+    # elif computer_score < human_score - human_score * 0.3:
+    #     while (computer_score + roll_total < human_score - human_score * 0.25
+    #            or roll_total > 11):
+    #         roll_score = roll_calc(roll())
+    #         if roll_score == 0:
+    #             print('The computer\'s score at the end of the turn is 0.')
+    #             return 0
+    #         roll_total += roll_score
+    #     print('The computer\'s score at the end of the turn is '
+    #           + str(roll_total) + '.')
+    #     return roll_total
+    else:
+        while roll_total < 17:
+            roll_score = roll_calc(roll())
+            if roll_score == 0:
+                print('The computer\'s score at the end of the turn is 0.')
+                return 0
+            roll_total += roll_score
+        print('The computer\'s score at the end of the turn is '
+              + str(roll_total) + '.')
+        return roll_total
+
+    # num_of_rolls = random.randint(1, 6)
+    # while num_of_rolls != 0:
+    #     roll_score = roll_calc(roll())
+    #     if roll_score == 0:
+    #         return roll_score
+    #         print('The computer\'s score at the end of the turn is 0.')
+    #     roll_total += roll_score
+    #     num_of_rolls -= 1
+    # print('The computer\'s score at the end of the turn is '
+    #       + str(roll_total) + '.')
+    # return roll_total
 
 
 # def computer_ai(computer_score, human_score):
 # This works out if True or False for next roll
-
-
 # def computer_ai_setting?
 # This works out if it'll play agrressive, conservative, or safe
 
@@ -36,8 +75,8 @@ def human_move(computer_score, human_score):
     """Tells the user both their current score and the computer's score, and
     how far behind (or ahead) they are. Then repeatedly asks whether the user
     wants to roll again."""
-    print('Your current score is ' + str(human_score) + ', and the computer\'s'
-          ' score is ' + str(computer_score) + '.')
+    print(' \nYour current score is ' + str(human_score) + ', and the '
+          'computer\'s score is ' + str(computer_score) + '.')
     diff = abs(computer_score - human_score)
     if human_score > computer_score:
         print('You\'re ahead by ' + str(diff) + '.')
@@ -45,14 +84,17 @@ def human_move(computer_score, human_score):
         print('You\'re behind by ' + str(diff) + '.')
     else:
         print('You\'re tied at ' + str(human_score) + '.')
+    print(' \nNow it\'s your turn:')
     new_turn = True
     roll_total = 0
     while new_turn:
         roll_score = roll_calc(roll())
         if roll_score == 0:
+            print('The score at the end of your turn is 0.')
             return roll_score
         roll_total += roll_score
         new_turn = ask_yes_or_no(input('Roll again? (y/n): '))
+    print('The score at the end of your turn is ' + str(roll_total) + '.')
     return roll_total
 
 
@@ -80,8 +122,8 @@ def ask_yes_or_no(prompt):
 
 
 def is_game_over(computer_score, human_score):
-    """Returns `True` if either player has 100 or more, and the players are
-    not tied, otherwise it returns `False`."""
+    """Returns True if either player has 100 or more, and the players are
+    not tied, otherwise it returns False."""
     if human_score != computer_score and (human_score >= 100 or
                                           computer_score >= 100):
         return True
@@ -100,8 +142,10 @@ def show_results(computer_score, human_score):
               + '. Beating the computer by ' + str(diff) + '.')
 
 
-def main(computer_score, human_score):
-    """This is where the program will start execution."""
+def main():
+    """This is where the program will be executed."""
+    computer_score = 0
+    human_score = 0
     instructions()
     while not is_game_over(computer_score, human_score):
         computer_score += computer_move(computer_score, human_score)
@@ -111,4 +155,4 @@ def main(computer_score, human_score):
 
 
 if __name__ == '__main__':
-    main(0, 0)
+    main()
